b59a934b9ee6b47c57e612144d2862790d6a8c6b
seenso <>
2022-03-03 10:32:55 -0900

    setup backend; now need to setup routes. Temp removed CONNECTION_URL in ./server/index.js.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,24 @@
 import express from 'express'; // using this instead of const express = require('express');
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import corse from 'corse';
+import cors from 'cors';
+
+const app = express(); // to init express app
+
+// setting up bodyParser to send requests
+app.use(bodyParser.json({limit: "30mb", extended: true})); // limiting image size as some can be large
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+//set up connection to db www.mongodb.com/cloud/atlas
+const CONNECTION_URL = "<url here>";
+const PORT = process.env.PORT || 5000; // use Heroku's port or port 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true}) // params to prevent console warnings
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log("ERROR", error.message));
+
+// mongoose.set('useFindAndModify', false); // makes sure we don't get any console warnings - DEPRECATED. no longer necesary. 
+// https://stackoverflow.com/questions/69030963/error-usefindandmodify-is-an-invalid-option
+
 
