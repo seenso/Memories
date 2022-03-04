402e1fe6afce0c472b23e979e7cb07d55c768bad
seenso <>
2022-03-03 16:16:38 -0900

    Created form.

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Memories
-An app to log all of your most imortant memories.
+A fullstack MERN app bundled in a monorepo to log all of your most imortant memories.
 
 # Dependencies installed
 - Backend: bodyParser, cors, express, mongoose
-- Frontend: axios (for making api requests), moment (for date formatting), react-file-base64 (to convert images), redux, redux-thunk (for async actions w redux), material-ui/core (UI kit)
+- Frontend: axios (for making api requests), moment (for date formatting), react-file-base64 (to convert images), redux, react-redux, redux-thunk (for async actions w redux), material-ui/core (UI kit)
+
+<!--
+  Resource: https://www.youtube.com/watch?v=ngc9gnGgUdA
+-->
--- server/index.js
+++ server/index.js
@@ -7,15 +7,16 @@ import postRoutes from './routes/posts.js';
 
 const app = express(); // to init express app
 
-app.use('/posts', postRoutes);
-
 // setting up bodyParser to send requests
 app.use(bodyParser.json({limit: "30mb", extended: true})); // limiting image size as some can be large
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
+// routes must go after app.use(corse());
+app.use('/posts', postRoutes);
+
 //set up connection to db www.mongodb.com/cloud/atlas
-const CONNECTION_URL = "<url here>";
+const CONNECTION_URL = "";
 const PORT = process.env.PORT || 5000; // use Heroku's port or port 5000
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true}) // params to prevent console warnings
